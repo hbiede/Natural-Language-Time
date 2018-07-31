9520fbc3dedac44c4e299e702d09105ca144dfd9
hbiede <hbiede@gmail.com>
2018-07-31 00:47:50 -0500

    Version 1.0

---
+++ Natural Language TimeTests/Natural_Language_TimeTests.swift
@@ -0,0 +1,54 @@
+//
+//  Natural_Language_TimeTests.swift
+//  Natural Langauge TimeTests
+//
+//  Created by Hundter Biede on 7/30/18.
+//  Copyright © 2018 Hundter Biede. All rights reserved.
+//
+
+import XCTest
+
+class Natural_Language_TimeTests: XCTestCase {
+
+    func testTimeString() {
+        var timeTest = Natural_Language_Time.NatTime()
+        
+        timeTest.timeString = "3-23"
+        XCTAssertEqual(timeTest.timeString, "3-23")
+
+        timeTest.timeString = "Fail String"
+        XCTAssertEqual(timeTest.timeString, "Default Time")
+
+        timeTest.timeString = "3-x"
+        XCTAssertEqual(timeTest.timeString, "Default Time")
+
+        timeTest.timeString = "x-23"
+        XCTAssertEqual(timeTest.timeString, "Default Time")
+    }
+
+    func testRoundArithmatic() {
+        XCTAssertEqual(Int(round(Double(0) / 5)) * 5, 0)
+        XCTAssertEqual(Int(round(Double(2) / 5)) * 5, 0)
+        XCTAssertEqual(Int(round(Double(4) / 5)) * 5, 5)
+        XCTAssertEqual(Int(round(Double(5) / 5)) * 5, 5)
+        XCTAssertEqual(Int(round(Double(12) / 5)) * 5, 10)
+        XCTAssertEqual(Int(round(Double(13) / 5)) * 5,15)
+        XCTAssertEqual(Int(round(Double(15) / 5)) * 5, 15)
+    }
+
+    func testNatLang() {
+        var timeTest = Natural_Language_Time.NatTime()
+
+        timeTest.timeString = "3-23"
+        XCTAssertEqual(timeTest.natLangString, "25 Past Three")
+
+        timeTest.timeString = "3-30"
+        XCTAssertEqual(timeTest.natLangString, "Half Past Three")
+
+        timeTest.timeString = "3-45"
+        XCTAssertEqual(timeTest.natLangString, "Quarter Til Four")
+
+        timeTest.timeString = "3-00"
+        XCTAssertEqual(timeTest.natLangString, "Three O'clock")
+    }
+}
